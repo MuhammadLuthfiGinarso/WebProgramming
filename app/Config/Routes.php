d01f481f12ff445fdb4f019bd6d3c87c79fa7ca8
Muhammad Luthfi Ginarso <32602200002@std.unissula.ac.id>
2024-07-12 15:46:48 +0700

    kedua

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/mata_kuliah', 'MataKuliah::index');
+$routes->get('/mata_kuliah/create', 'MataKuliah::create');
+$routes->post('/mata_kuliah/store', 'MataKuliah::store');
+$routes->get('/mata_kuliah/edit/(:segment)', 'MataKuliah::edit/$1');
+$routes->post('/mata_kuliah/update/(:segment)', 'MataKuliah::update/$1');
+$routes->get('/mata_kuliah/delete/(:segment)', 'MataKuliah::delete/$1');
